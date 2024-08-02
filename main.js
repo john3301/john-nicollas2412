2771b435a3acb969ef7a7238bcbb916b16adb7b9
john3301 <127457637+john3301@users.noreply.github.com>
2024-08-01 22:30:27 -0300

    Add files via upload
    hmtkdky c6r

---
+++ main.js
@@ -0,0 +1,14 @@
+const botoes = document.querySelectorAll("botao")
+const botoes = document.querySelectorAll(".aba-conteudo");
+
+for (let i = 0; j < botoes.length; i ++) {
+    botoes[i].onclick = function () {
+
+for (let j = 0; j <botoes.length; j++) {
+    botoes[j].classList.remove("ativo");
+    textos[j].classList.remove("ativos");
+
+}
+    
+    
+}
